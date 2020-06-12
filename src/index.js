5861917a118deac849406777f99d746a3246b93c
dorapon333 <49771853+dorapon333@users.noreply.github.com>
2020-06-12 16:28:14 +0900

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { ResponsiveNetwork } from '@nivo/network'
 
 
-const App = () => {
+const NetworkChart = () => {
   const [data, setData] = useState({nodes : [], links: []})
   useEffect(() => {
     window
@@ -11,15 +11,16 @@ const App = () => {
     .then((response) => response.json())
     .then((data) =>{
       for (const node of data.nodes) {
-        node.color = "blue";
+
+        //node.color = "blue";
         node.id = node.id.toString();
-        node.radius = 8;      
+        node.radius = 5;      
       }
       for (const link of data.links ){
          link.color = "gray";  
          link.source = link.source.toString();//data.nodes[link.source].id
          link.target = link.target.toString();//data.nodes[link.target].id
-         link.distance = 30;
+         link.distance = 20;
       }
       setData(data)
     })
@@ -34,12 +35,14 @@ const App = () => {
 
  function check(value){
      for(const links of newLinks){
-       if(value.id === (links.source || links.target) ){  
+       if(  (value.id === links.source) || (value.id === links.target)){  
          return value
        }
      }
    }
   const newNodes = data.nodes.filter(check)
+
+  
 
 
    console.log(newNodes)
@@ -47,18 +50,47 @@ const App = () => {
 
 
 return (
-    <div style={{width: '800px', height: '800px'}} >
-      <ResponsiveNetwork        
-        nodes={newNodes}    
-        links={newLinks}
-        margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-        repulsivity={6}
-        iterations={30}
-        nodeColor={function(t){return t.color}}
-        nodeBorderWidth={1}
-        nodeBorderColor={{ from: 'color', modifiers: [ [ 'darker', 0.8 ] ] }}
-        linkThickness={function(t){return 2*(2-t.source.depth)}}
-        motionStiffness={160}
-        motionDamping={12}      />
-    </div>  )}
+  <div style={ {"width": "1000px", "height" : "1000px"}} >
+    <ResponsiveNetwork        
+      nodes={newNodes}    
+      links={newLinks}
+      margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+      repulsivity={3}
+      iterations={50}
+      nodeColor={(node) =>{
+        const t = 20;
+        if ( node.values[t] === 1 ){
+          return "blue"
+        }else if( node.values[t]  === 0){
+          return "gray"
+        }else if( node.values[t] === -1){
+          return "red"
+        }
+      }}
+
+      linkColor={(link) =>{
+        return "gray"
+      }}
+      
+      
+      
+      nodeBorderWidth={1}
+      nodeBorderColor={{ from: 'color', modifiers: [ [ 'darker', 0.8 ] ] }}
+      linkThickness={(link) =>{
+        return 2
+      }}
+      motionStiffness={160}
+      motionDamping={12}      />
+    </div> 
+  )
+}
+
+
+const App = () => {
+  return(
+    <div>
+      <NetworkChart/>   
+    </div>
+  )
+}
 render(<App />, document.querySelector('#content'))
